3855b8ff0f4d65590e27c3276fc9a0c662480c85
anh.do10 <anh.do10@onemount.com>
2026-04-01 19:53:05 +0700

    Get private ipv4 ec2

--- src/main/java/src/Application.java
+++ src/main/java/src/Application.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
 
 @SpringBootApplication
 @RestController
@@ -15,6 +18,16 @@ public class Application {
 
     @GetMapping("")
     public String hello() {
-        return "Hello World!";
+        return "Hello World: " + this.getPrivateIp();
+    }
+
+    public String getPrivateIp() {
+        try {
+            URL url = new URL("http://169.254.169.254/latest/meta-data/local-ipv4");
+            BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+            return br.readLine();
+        } catch (Exception e) {
+            return "Cannot get IP";
+        }
     }
 }
